c3caee5939a97e66a580efc945619803255715c8
Kris Nova <kris@nivenly.com>
2017-07-06 22:29:16 -0600

    Pushing up changes - switching to mac book

--- cloud/amazon/reconciler.go
+++ cloud/amazon/reconciler.go
@@ -6,6 +6,9 @@ import (
 	"github.com/kris-nova/kubicorn/cloud/amazon/awsSdkGo"
 	"github.com/kris-nova/kubicorn/cloud/amazon/resources"
 	"github.com/kris-nova/kubicorn/logger"
+	"strings"
+	"time"
+	"github.com/kris-nova/kubicorn/cutil/hang"
 )
 
 type Reconciler struct {
@@ -62,6 +65,29 @@ func (r *Reconciler) GetExpected() (*cluster.Cluster, error) {
 	return expectedCluster, nil
 }
 
+func cleanUp(cluster *cluster.Cluster, i int) error {
+	for j := i - 1; i >= 0; i-- {
+		resource := model[j]
+		actualResource, err := resource.Actual(cluster)
+		if err != nil {
+			err, i = destroyI(err, j)
+			if err != nil {
+				return err
+			}
+			continue
+		}
+		err = resource.Delete(actualResource)
+		if err != nil {
+			err, i = destroyI(err, j)
+			if err != nil {
+				return err
+			}
+			continue
+		}
+	}
+	return nil
+}
+
 func (r *Reconciler) Reconcile(actualCluster, expectedCluster *cluster.Cluster) (*cluster.Cluster, error) {
 	newCluster := newClusterDefaults(r.Known)
 	for i := 0; i < len(model); i++ {
@@ -76,7 +102,13 @@ func (r *Reconciler) Reconcile(actualCluster, expectedCluster *cluster.Cluster) 
 		}
 		appliedResource, err := resource.Apply(actualResource, expectedResource, newCluster)
 		if err != nil {
-			return nil, err
+			logger.Critical("Error during apply! Attempting cleaning: %v", err)
+			err = cleanUp(actualCluster, i)
+			if err != nil {
+				logger.Critical("Failure during cleanup! Abandoned resources!")
+				return nil, err
+			}
+			return nil,  nil
 		}
 		newCluster, err = resource.Render(appliedResource, newCluster)
 		if err != nil {
@@ -86,22 +118,52 @@ func (r *Reconciler) Reconcile(actualCluster, expectedCluster *cluster.Cluster) 
 	return newCluster, nil
 }
 
+var destroyRetryStrings = []string{
+	"DependencyViolation:",
+}
+
+var hg = &hang.Hanger{
+	Ratio: 1,
+}
+
+func destroyI(err error, i int) (error, int) {
+	hg.Hang()
+	for _, retryString := range destroyRetryStrings {
+		if strings.Contains(err.Error(), retryString) {
+			logger.Debug("Retry failed delete: %v", err)
+			time.Sleep(1 * time.Second)
+			return nil, i + 1
+		}
+	}
+	return err, 0
+}
+
+
+
 func (r *Reconciler) Destroy() error {
 	for i := len(model) -1; i >= 0; i-- {
 		resource := model[i]
 		actualResource, err := resource.Actual(r.Known)
 		if err != nil {
-			return err
+			err, i = destroyI(err, i)
+			if err != nil {
+				return err
+			}
+			continue
 		}
 		err = resource.Delete(actualResource)
 		if err != nil {
-			logger.Warning("Delete Failure: %v Retrying", err)
-			i++
+			err, i = destroyI(err, i)
+			if err != nil {
+				return err
+			}
 			continue
 		}
 	}
 	return nil
 }
+
+
 
 func newClusterDefaults(base *cluster.Cluster) *cluster.Cluster {
 	new := &cluster.Cluster{
