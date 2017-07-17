94f6cac2a66403889a80bb8ebb160f9dd2173763
Kris Nova <kris@nivenly.com>
2017-07-17 01:58:13 -0600

    Fixing go get

--- cutil/initapi/ssh.go
+++ cutil/initapi/ssh.go
@@ -1,7 +1,8 @@
 package initapi
 
 import (
-	"github.com/gravitational/teleport/lib/sshutils"
+	//"github.com/gravitational/teleport/lib/sshutils"
+	//"github.com/gravitational/teleport/lib/sshutils"
 	"github.com/kris-nova/klone/pkg/local"
 	"github.com/kris-nova/kubicorn/apis/cluster"
 	"io/ioutil"
@@ -15,11 +16,11 @@ func ssh(initCluster *cluster.Cluster) (*cluster.Cluster, error) {
 		}
 		initCluster.Ssh.PublicKeyData = bytes
 
-		fp, err := sshutils.PrivateKeyFingerprint(bytes)
-		if err != nil {
-			return nil, err
-		}
-		initCluster.Ssh.PublicKeyFingerprint = fp
+		//fp, err := sshutils.PrivateKeyFingerprint(bytes)
+		//if err != nil {
+		//	return nil, err
+		//}
+		//initCluster.Ssh.PublicKeyFingerprint = fp
 	}
 
 	return initCluster, nil
