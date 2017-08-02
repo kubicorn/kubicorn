f4d8e68e417a7066e4037f9de6689a68d6f324fe
Peter Smeets <p.e.smeets@gmail.com>
2017-08-02 18:19:46 +0200

    Added remove image option

--- docker/build.sh
+++ docker/build.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
+VERBOSE=FALSE
 VERBOSE_DOCKER_RUN=""
 VERBOSE_DOCKER_BUILD="-q"
+REMOVE_IMAGE=FALSE
 
 for i in "$@"
 do
 case $i in
     -v|--verbose)
+    VERBOSE=TRUE
     VERBOSE_DOCKER_RUN="-e 'VERBOSE=1'"
     VERBOSE_DOCKER_BUILD=""
+    shift # past argument=value
+    ;;
+    --remove-image)
+    REMOVE_IMAGE=TRUE
     shift # past argument=value
     ;;
 esac
 done
 
-#echo removing old container if exists
-#docker image rm gobuilder
+if ${REMOVE_IMAGE} ; then
+    echo removing old container if exists
+    docker image rm gobuilder
+fi
 echo Building container
 docker build -t gobuilder "$(pwd)" ${VERBOSE_DOCKER_BUILD}
 echo Running make script
