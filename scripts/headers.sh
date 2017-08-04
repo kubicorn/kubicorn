c81609ad78984ff21bd05bcf61894e8907532076
Marko Mudrinić <mudrinic.mare@gmail.com>
2017-08-04 10:13:31 +0200

    scripts: fix headers generation

--- scripts/headers.sh
+++ scripts/headers.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-read -r -d '' LICENSE <<EOF
+read -r -d '' EXPECTED <<EOF
 // Copyright © 2017 The Kubicorn Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,11 +30,19 @@ read -r -d '' LICENSE <<EOF
 // limitations under the License.
 EOF
 
+read -r -d '' LICENSE <<EOF
+// Copyright © 2017 The Kubicorn Authors\n//\n// Licensed under the Apache License, Version 2.0 (the "License");\n// you may not use this file except in compliance with the License.\n// You may obtain a copy of the License at\n//\n//\ \ \ \ \ http://www.apache.org/licenses/LICENSE-2.0\n//\n// Unless required by applicable law or agreed to in writing, software\n// distributed under the License is distributed on an "AS IS" BASIS,\n// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n// See the License for the specific language governing permissions and\n// limitations under the License.\n
+EOF
+
 FILES=$(find . -name "*.go" -not -path "./vendor/*")
 
 for FILE in $FILES; do
+	if [ "$FILE" == "./bootstrap/bootstrap.go" ]; then
+            continue
+        fi
+
         CONTENT=$(head -n 13 $FILE)
-        if [ "$CONTENT" != "$LICENSE" ]; then
+        if [ "$CONTENT" != "$EXPECTED" ]; then
 		ESCAPED=$(echo $LICENSE | awk '{printf "%s\\n", $0}')
 		sed -i "1s|^|${ESCAPED}\n|" $FILE
         fi
