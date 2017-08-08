4ad3f8aa8d2ace9c38432b731b06b3693cfdeccb
Martin Etmajer <martin@etmajer.com>
2017-08-08 23:15:11 +0200

    Remove duplicate definition of func Log() (a relict of previous merge).

--- cutil/logger/logger.go
+++ cutil/logger/logger.go
@@ -136,10 +136,6 @@ func Warning(format string, a ...interface{}) {
 	}
 }
 
-func Log(format string, a ...interface{}) {
-	fmt.Printf(format, a...)
-}
-
 func label(format, label string) string {
 	t := time.Now()
 	rfct := t.Format(time.RFC3339)
