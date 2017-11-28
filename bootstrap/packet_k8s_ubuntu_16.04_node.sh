0cc53babf1d515ffae116ec49b9ca4b1713def7d
Avi Deitcher <avi@deitcher.net>
2017-11-28 10:29:29 +0200

    Fix apt order in packet bootstrap files

--- bootstrap/packet_k8s_ubuntu_16.04_node.sh
+++ bootstrap/packet_k8s_ubuntu_16.04_node.sh
@@ -4,12 +4,22 @@
 # and the shell script real work. If you need conditional logic, write it in bash or make another shell script.
 # ------------------------------------------------------------------------------------------------------------------------
 
+
+# order is important:
+# 1. update
+# 2. install apt-transport-https
+# 3. add kubernetes repos to list
+# 4. update again
+# 5. install
+apt-get update -y
+apt-get install -y apt-transport-https
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 touch /etc/apt/sources.list.d/kubernetes.list
-sh -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list'
+sh -c 'echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list'
 
-apt-get install apt-transport-https
 apt-get update -y
+
 apt-get install -y \
     socat \
     ebtables \
