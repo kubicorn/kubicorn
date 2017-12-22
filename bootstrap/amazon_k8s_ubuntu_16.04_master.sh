241f4d02ca4c0b3f766e6a3486e5cf4297fe2445
Kris Nova <kris@nivenly.com>
2017-12-22 09:23:20 -0700

    Update amazon_k8s_ubuntu_16.04_master.sh

--- bootstrap/amazon_k8s_ubuntu_16.04_master.sh
+++ bootstrap/amazon_k8s_ubuntu_16.04_master.sh
@@ -4,6 +4,9 @@
 # and the shell script real work. If you need conditional logic, write it in bash or make another shell script.
 # ------------------------------------------------------------------------------------------------------------------------
 
+# Specify the Kubernetes version to use
+KUBERNETES_VERSION="1.8.4-00"
+
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 touch /etc/apt/sources.list.d/kubernetes.list
 sh -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list'
@@ -25,7 +28,7 @@ apt-get install -y \
     docker.io \
     apt-transport-https \
     kubelet \
-    kubeadm=1.8.4-00 \
+    kubeadm=${KUBERNETES_VERSION} \
     cloud-utils \
     jq
 
@@ -46,6 +49,7 @@ apiVersion: kubeadm.k8s.io/v1alpha1
 kind: MasterConfiguration
 cloudProvider: aws
 token: ${TOKEN}
+kubernetesVersion: ${KUBERNETES_VERSION}
 nodeName: ${HOSTNAME}
 api:
   advertiseAddress: ${PUBLICIP}
