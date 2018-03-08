fe7f02fc2d0899bf583bc038708ca6dd79027b20
Jason DeTiberus <detiber@gmail.com>
2018-03-08 15:32:57 -0500

    test

--- pkg/state/fs/impl_test.go
+++ pkg/state/fs/impl_test.go
@@ -20,10 +20,10 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/ghodss/yaml"
 	"github.com/kubicorn/kubicorn/apis/cluster"
 	"github.com/kubicorn/kubicorn/pkg/state"
 	"github.com/kubicorn/kubicorn/profiles/amazon"
-	"github.com/kubicorn/kubicorn/vendor/github.com/ghodss/yaml"
 )
 
 func TestStateFileSystem(t *testing.T) {
