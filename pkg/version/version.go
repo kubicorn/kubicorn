2abd196388ac0d3ba0a49bc0d768f732cecc9d5d
Marko Mudrinić <mudrinic.mare@gmail.com>
2018-03-27 22:29:20 +0200

    *: version review

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,3 +1,17 @@
+// Copyright © 2017 The Kubicorn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package version
 
 import (
@@ -15,28 +29,28 @@ var (
 	GitSha string
 
 	// The Version of the program from the VERSION file (automatically set at compile time)
-	kubicornVersion string
+	KubicornVersion string
 )
 
 // Version represents Kubicorn version.
 type Version struct {
-	Version string
+	Version   string
 	GitCommit string
 	BuildDate string
 	GoVersion string
-	GOOS string
-	GOArch string
+	GOOS      string
+	GOArch    string
 }
 
 // GetVersion returns Kubicorn version.
 func GetVersion() *Version {
-	return &Version {
-		Version: kubicornVersion,
+	return &Version{
+		Version:   KubicornVersion,
 		GitCommit: GitSha,
 		BuildDate: time.Now().UTC().String(),
 		GoVersion: runtime.Version(),
-		GOOS: runtime.GOOS,
-		GOArch: runtime.GOARCH,
+		GOOS:      runtime.GOOS,
+		GOArch:    runtime.GOARCH,
 	}
 }
 
