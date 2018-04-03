604a512d790f82cc26a0448b63bebbf6db3d8954
liz <liz@heptio.com>
2018-04-03 11:45:33 -0400

    Remove unneeded constants

--- test/amazon/cluster_test.go
+++ test/amazon/cluster_test.go
@@ -30,13 +30,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-const (
-	APISleepSeconds   = 5
-	APISocketAttempts = 40
-)
-
-var testCluster *cluster.Cluster
-
 func TestAmazon(t *testing.T) {
 	logger.TestMode = true
 	logger.Level = 4
